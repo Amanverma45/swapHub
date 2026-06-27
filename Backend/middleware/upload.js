f80d22a3c6288c0c85169abea5cb690ad5907ed1
Aman Verma <av478136@gmail.com>
2026-06-27 11:36:21 +0530

    update upload.js error to Error

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -12,19 +12,36 @@ const storage = new CloudinaryStorage({
 })
 const upload = multer({
     storage,
-    fileFilter:(req,file,cb)=>{
-        const allowedTypes = [
-            'Image/jpeg',
-            'Image/jpg',
-            'Image/png',
-            'Image/webp',
-        ];
-        if(allowedTypes.includes(file.mimetype)){
-            cb(null,true);
-        }else{
-            cb(new Error("only JPG, JPEG, PNG and WEBP images are allowed"),false);
-        }
-    },
+    // fileFilter:(req,file,cb)=>{
+    //     const allowedTypes = [
+    //         'Image/jpeg',
+    //         'Image/jpg',
+    //         'Image/png',
+    //         'Image/webp',
+    //     ];
+    //     if(allowedTypes.includes(file.mimetype)){
+    //         cb(null,true);
+    //     }else{
+    //         cb(new Error("only JPG, JPEG, PNG and WEBP images are allowed"),false);
+    //     }
+    // },
+    fileFilter: (req, file, cb) => {
+  console.log("Mimetype:", file.mimetype);
+
+  const allowedTypes = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/webp",
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    console.log("Rejected:", file.mimetype);
+    cb(new Error("Only JPG, JPEG, PNG and WEBP images are allowed"), false);
+  }
+},
     limits: {
         fileSize: 5* 1024 * 1024, //5MB
     }
