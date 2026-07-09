1f66d054bcaa79918985f656b34b6b1d78293f53
Aman Verma <av478136@gmail.com>
2026-07-09 17:05:36 +0530

    UI polish

--- Frontend/hub/src/Components/SwapRequests.jsx
+++ Frontend/hub/src/Components/SwapRequests.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import axios from '../utils/axiosInstance';
 import toast from 'react-hot-toast';
+import { MdLocationOn } from "react-icons/md";
 
 const SwapRequests = () => {
   const [requests, setRequests] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [previewImage, setPreviewImage] = useState(null);
 
   const getSwapRequests = async () => {
     try {
@@ -47,141 +49,166 @@ const SwapRequests = () => {
     getSwapRequests();
   }, []);
   return (
-    <section className="w-[90%] max-w-7xl mx-auto py-16">
+    <>
+      <section className="w-[90%] max-w-7xl mx-auto py-16">
 
-      <div className="mb-10">
-        <h1 className="text-4xl font-bold">
-          Swap Requests
-        </h1>
+        <div className="mb-10">
+          <h1 className="text-4xl font-bold">
+            Swap Requests
+          </h1>
 
-        <p className="text-gray-600 mt-2">
-          Manage all incoming swap requests.
-        </p>
-      </div>
+          <p className="text-gray-600 mt-2">
+            Manage all incoming swap requests.
+          </p>
+        </div>
 
-      {loading ? (
-        <h2 className="text-center text-xl">
-          Loading...
-        </h2>
-      ) : requests.length === 0 ? (
-        <h2 className="text-center text-xl">
-          No Swap Requests
-        </h2>
-      ) : (
-        <div className="space-y-8">
-          {requests.map((request) => (
-            <div
-              key={request._id}
-              className="bg-white rounded-3xl shadow-md border p-6">
-              <div className="grid grid-cols-1 sm:grid-cols-[1fr_auto] gap-3 mb-6 items-start">
+        {loading ? (
+          <h2 className="text-center text-xl">
+            Loading...
+          </h2>
+        ) : requests.length === 0 ? (
+          <h2 className="text-center text-xl">
+            No Swap Requests
+          </h2>
+        ) : (
+          <div className="space-y-8">
+            {requests.map((request) => (
+              <div
+                key={request._id}
+                className="bg-white rounded-xl h-[500px] md:min-h-[650px] shadow-lg border border-gray-200 overflow-hidden hover:-translate-y-1 hover:shadow-xl transition-all duration-300">
+                <div className="grid grid-cols-1 md:grid-cols-[1fr_auto] gap-3 md:gap-2 items-start">
 
-                <div className="flex-1 min-w-0">
-                  <h2 className="text-2xl font-bold">
-                    {request.sender.name}
-                  </h2>
+                  <div className="bg-green-50 p-3 md:p-5 md:h-35 md:w-390 border-b border-green-100 rounded-t-xl">
+                    <h2 className="text-xl md:text-2xl font-bold text-gray-800">
+                      {request.sender.name}
+                    </h2>
 
-                  <p className="text-gray-500 break-all">
-                    {request.sender.email}
-                  </p>
+                    <p className="text-sm md:text-base text-gray-500 break-all mt-1">
+                      {request.sender.email}
+                    </p>
+
+                    <div className="px-3 md:px-5 pt-2 md:pt-4">
+                      <span
+                        className={`inline-flex items-center -ml-4  px-4 py-2 rounded-full text-xs md:text-sm md:-ml-5 font-semibold ${request.status === "accepted"
+                          ? "bg-green-100 text-green-700"
+                          : request.status === "rejected"
+                            ? "bg-red-100 text-red-700"
+                            : "bg-yellow-100 text-yellow-700"
+                          }`}
+                      >
+                        {request.status === "pending"
+                          ? "🟡 Pending"
+                          : request.status === "accepted"
+                            ? "🟢 Accepted"
+                            : "🔴 Rejected"}
+                      </span>
+                    </div>
+                  </div>
+
                 </div>
 
-                <span
-                  className={`shrink-0 inline-flex items-center gap-2 px-4 py-2 rounded-full text-sm font-semibold
-    ${request.status === "accepted"
-                      ? "bg-green-100 text-green-700"
-                      : request.status === "rejected"
-                        ? "bg-red-100 text-red-700"
-                        : "bg-yellow-100 text-yellow-700"
-                    }`}
-                >
-                  <span
-                    className={`w-2.5 h-2.5 rounded-full
-      ${request.status === "accepted"
-                        ? "bg-green-600"
-                        : request.status === "rejected"
-                          ? "bg-red-600"
-                          : "bg-yellow-500"
-                      }`}
-                  ></span>
+                <div className="flex justify-center gap-2 mt-4 md:gap-8 md:py-2 ">
 
-                  {request.status.charAt(0).toUpperCase() + request.status.slice(1)}
-                </span>
+                  {/* Requested Product */}
+                  <div className="border border-gray-200 w-42 h-70 md:w-132 md:h-[390px] md:p-4 -mt-2 md:mt-0 rounded-xl hover:shadow-xl transition-all duration-300">
 
+                    <h3 className="font-bold ml-2 mt-1 md:ml-0 text-sm md:text-lg mb-4">
+                      Requested Product
+                    </h3>
+
+                    <img
+                      src={request.requestedProduct.image}
+                      onClick={() => setPreviewImage(request.requestedProduct.image)}
+                      className="w-36 h-36 md:w-125 md:rounded-none md:h-54 object-cover md:ml-0 ml-3 md:mx-auto rounded-lg cursor-pointer"
+                    />
+
+                    <h4 className="text-sm md:text-lg font-semibold ml-2 md:ml-0 mt-4">
+                      {request.requestedProduct.productName}
+                    </h4>
+
+                    <p className="mt-2 ml-2 md:ml-0 inline-block bg-green-100 text-green-700 rounded-xl px-3 py-1 text-xs md:text-sm">
+                      {request.requestedProduct.category}
+                    </p>
+
+                    <p className="flex items-center gap-1 mt-2 ml-2 md:ml-0 text-sm text-gray-600">
+                      <MdLocationOn className="text-[#2E7D32]" />
+                      {request.requestedProduct.location}
+                    </p>
+
+                  </div>
+
+                  {/* Offered Product */}
+                  <div className="border border-gray-200 w-42 h-70 md:w-132 md:h-[390px] md:p-4 -mt-2 md:mt-0 rounded-xl hover:shadow-xl transition-all duration-300">
+
+                    <h3 className="font-bold ml-2 mt-1 md:ml-0 text-sm md:text-lg mb-4">
+                      Offered Product
+                    </h3>
+
+                    <img
+                      src={request.offeredProduct.image}
+                      onClick={() => setPreviewImage(request.offeredProduct.image)}
+                      className="w-36 h-36 md:w-125 md:rounded-none md:h-54 object-cover md:ml-0 ml-3 md:mx-auto rounded-lg cursor-pointer"
+                    />
+
+                    <h4 className="text-sm md:text-lg font-semibold ml-2 md:ml-0 mt-4">
+                      {request.offeredProduct.productName}
+                    </h4>
+
+                    <p className="mt-2 ml-2 md:ml-0 inline-block bg-green-100 text-green-700 rounded-xl px-3 py-1 text-xs md:text-sm">
+                      {request.offeredProduct.category}
+                    </p>
+
+                    <p className="flex items-center gap-1 mt-2 ml-2 md:ml-0 text-sm text-gray-600">
+                      <MdLocationOn className="text-[#2E7D32]" />
+                      {request.offeredProduct.location}
+                    </p>
+
+                  </div>
+
+                </div>
+                {request.status === "pending" && (
+                  <div className="flex justify-center gap-2 md:gap-4 mt-5 md:mt-6 pb-4">
+                    <button
+                      onClick={() => handleAccept(request._id)}
+                      className="w-36 md:w-40 bg-[#2E7D32] hover:bg-[#256728] text-white py-2 md:py-3 rounded-xl transition font-medium"
+                    >
+                      Accept
+                    </button>
+
+                    <button
+                      onClick={() => handleReject(request._id)}
+                      className="w-36 md:w-40 bg-red-500 hover:bg-red-600 text-white py-2 md:py-3 rounded-xl transition font-medium"
+                    >
+                      Reject
+                    </button>
+                  </div>
+                )}
               </div>
+            ))}
+          </div>
+        )}
+      </section>
+      {previewImage && (
+        <div
+          className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
+          onClick={() => setPreviewImage(null)}
+        >
+          <button
+            onClick={() => setPreviewImage(null)}
+            className="absolute top-5 right-5 text-white text-4xl"
+          >
+            ✕
+          </button>
 
-              <div className="grid md:grid-cols-2 gap-8">
-                <div className="border rounded-2xl p-4">
+          <img
+            src={previewImage}
+            className="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+            onClick={(e) => e.stopPropagation()}
+          />
 
-                  <h3 className="font-bold text-lg mb-4">
-                    Requested Product
-                  </h3>
-
-                  <img
-                    src={request.requestedProduct.image}
-                    alt={request.requestedProduct.productName}
-                    className="w-full h-52 object-cover rounded-xl"
-                  />
-
-                  <h4 className="text-xl font-semibold mt-4">
-                    {request.requestedProduct.productName}
-                  </h4>
-
-                  <p className="text-gray-600">
-                    {request.requestedProduct.category}
-                  </p>
-
-                  <p className="text-gray-600">
-                    📍 {request.requestedProduct.location}
-                  </p>
-
-                </div>
-
-
-                <div className="border rounded-2xl p-4">
-
-                  <h3 className="font-bold text-lg mb-4">
-                    Offered Product
-                  </h3>
-
-                  <img
-                    src={request.offeredProduct.image}
-                    alt={request.offeredProduct.productName}
-                    className="w-full h-52 object-cover rounded-xl"
-                  />
-
-                  <h4 className="text-xl font-semibold mt-4">
-                    {request.offeredProduct.productName}
-                  </h4>
-
-                  <p className="text-gray-600">
-                    {request.offeredProduct.category}
-                  </p>
-
-                  <p className="text-gray-600">
-                    📍 {request.offeredProduct.location}
-                  </p>
-                </div>
-              </div>
-              {request.status === "pending" && (
-                <div className="flex justify-end gap-4 mt-6">
-                  <button
-                    onClick={() => handleAccept(request._id)}
-                    className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-xl transition">
-                    Accept
-                  </button>
-
-                  <button
-                    onClick={() => handleReject(request._id)}
-                    className="bg-red-500 hover:bg-red-600 text-white px-6 py-2 rounded-xl transition">
-                    Reject
-                  </button>
-                </div>
-              )}
-            </div>
-          ))}
         </div>
       )}
-    </section>
+    </>
   );
 };
 
